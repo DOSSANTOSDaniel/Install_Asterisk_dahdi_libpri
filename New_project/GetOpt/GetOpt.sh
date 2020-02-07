c9885355764ad37d13a936bbec548dead6ffe8bc
DOSSANTOSDaniel <daniel.massy91@gmail.com>
2020-02-07 09:36:50 +0100

    modification mineur

--- New_project/GetOpt/GetOpt.sh
+++ New_project/GetOpt/GetOpt.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+# Lit une option et place cela dans la variable $option
+while getopts ":d :l :f" option
+do
+  case ${option} in
+    d ) echo "Install Dahdi";;
+    l ) echo "Install Libpri";;
+    f ) echo "Full Instal";;
+    \? ) echo "Error use: [-d] or [-l] or [-f]";;
+  esac
+done
