1f2b87e763a19c0db6b571f0c3e08ddcfbc80acc
DOS SANTOS <danielitto91@gmail.com>
2020-02-12 22:12:01 +0100

    Update Usage.sh

--- New_project/Usage/Usage.sh
+++ New_project/Usage/Usage.sh
@@ -3,16 +3,17 @@
 declare -r prog="$0"
 usage() {
     cat <<USAGE
-	Usage: ${prog} -[optionX]
-	Option 1:   -f --full	Installation de Dahdi et Libpri.
-	Option 2:   -d --dahdi	Installation de Dahdi.
-	Option 4:   -n --noint  Installation d'asterisk non intéractive.
-	Option 3:   -h --help   Aide
+	Usage: ${prog} -[option]
+	Option 1:   -f --full	Installation des modules Dahdi et Libpri.
+	Option 2:   -d --dahdi	Installation du module Dahdi seule.
+	Option 3:   -n --noint  Installation d'asterisk non intéractive.
+	Option 4:   -h --help   Aide
 
-	Exemple 1:  ${prog}  -f		Installation des modules Dahdi et Libpri.	
-	Exemple 2:  ${prog}  -d		Installation du module Dahdi seule.
-	Exemple 2:  ${prog}  -h		Affiche l'aide.
-	Exemple 2:  ${prog}		Installation d'Asterisk sans les modules Dahdi et Libpri.
+	Exemple 1:  ${prog}  -f		Installation de Dahdi et Libpri.	
+	Exemple 2:  ${prog}  -d		Installation de Dahdi.
+	Exemple 3:  ${prog}		Installation d'Asterisk sans les modules Dahdi et Libpri.
+	Exemple 4:  ${prog}  -n		Installation d'asterisk non intéractive.
+	Exemple 5:  ${prog}  -h		Affiche l'aide.	
 USAGE
 }
 usage
