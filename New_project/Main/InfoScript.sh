2f4f75468269abcf193152004fcdcfba66a6d451
DOS SANTOS <danielitto91@gmail.com>
2020-02-13 01:45:36 +0100

    Create InfoScript.sh

---
+++ New_project/Main/InfoScript.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#===============================================================================
+#
+#          FILE:  test.sh
+#
+#         USAGE:  ./test.sh
+#
+#   DESCRIPTION:
+#
+#       OPTIONS:  -i full     
+#                 -i dahdi
+#                 -i noint
+#                 -h
+#                 -v
+#
+#
+#
+#  REQUIREMENTS:  ---
+#          BUGS:  ---
+#         NOTES:  ---
+#        AUTHOR:  DOS SANTOS Daniel daniel.massy91@gmail.com
+#       VERSION:  1.0
+#       CREATED:  12/02/2020
+#===============================================================================
