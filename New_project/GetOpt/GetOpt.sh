3e384670bd603e9c7b8e7df10d8b844048704306
DOS SANTOS <danielitto91@gmail.com>
2020-02-14 12:00:36 +0100

    Update GetOpt.sh

--- New_project/GetOpt/GetOpt.sh
+++ New_project/GetOpt/GetOpt.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-if (($# > 2))
+if (( $# > 2 ))
 then
   echo "trop d'arguments !"
   exit 1
+elif [[ $1 =~ ^[^-.] ]]
+then
+  echo "$1 n'est pas un option! "
 fi
  
-while getopts ":i:h :v" opt; do
+while getopts ":i: :h :v" opt
+do
   case $opt in
     i)
       case $OPTARG in
@@ -32,12 +36,13 @@ while getopts ":i:h :v" opt; do
       echo "-v version du script" >&2
       ;;
     \?)
-      echo "Cette option est invalide: -$OPTARG" >&2
+      echo "Cette option est invalide: $OPTARG" >&2
       exit 1
       ;;
     :)
-      echo "Option -$OPTARG requires an argument." >&2
+      echo "L'option $OPTARG nécessite un argument." >&2
       exit 1
       ;;
   esac
 done
+
