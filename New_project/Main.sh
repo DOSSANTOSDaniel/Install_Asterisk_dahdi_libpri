afad3354b555598ace4fdb784e6cd6653e4dba6e
DOSSANTOSDaniel <daniel.massy91@gmail.com>
2020-03-04 17:25:55 +0100

    refonte

--- New_project/Main.sh
+++ New_project/Main.sh
@@ -21,10 +21,13 @@
 #       CREATED:  12/02/2020
 #===============================================================================
 
-### Déclaration de variables ###
-declare -r User=$(w | awk '{print $1}' | awk 'NR==3')
+set -e
 
-### Variables ###
+### Déclaration de variables ###
+# Variables
+declare -r CountryCode="33"
+declare -r IpCheck="$(hostname -i | awk '{print $1}')"
+declare -r User=$(w | awk '{print $1}' | awk 'NR==3')
 declare -r Ver='1.0'
 declare -r Prog="$0"
 declare SipUsers
@@ -35,20 +38,16 @@ declare -r KeyGpgAst='0x5D984BE337191CE7'
 declare -r VerDahdi='dahdi-linux-complete-3.1.0+3.1.0'
 # Libpri
 declare -r VerLibpri='libpri-1.6.0'
-#Déclaration des variables de couleur
+# Couleurs
 declare -r Green='\e[1;32m'
 declare -r Neutral='\e[0;m'
 declare -r Red='\e[0;31m'
-#Récupération IP
-declare -r IpCheck="$(hostname -i | awk '{print $1}')"
-
-#Téléchargements
+# Téléchargements
 declare -r DownloadDahdi="https://downloads.asterisk.org/pub/telephony/dahdi-linux-complete/releases/"
 declare -r DownloadLibpri="https://downloads.asterisk.org/pub/telephony/libpri/releases/"
 declare -r DownloadAsterisk="http://downloads.asterisk.org/pub/telephony/asterisk/releases/"
 
 ### Déclaration des fonctions ###
-
 usage() {
     cat <<USAGE
 	Usage: ${prog} -option [arg] 
@@ -81,7 +80,6 @@ Ver
 }
 
 Interface() {
-
 declare -i imi=0
 declare -i choix=0
 declare -i exitstatus=0
@@ -112,8 +110,7 @@ do
         whiptail --title "Creation des comptes SIP" --msgbox "Vous avez annulez" 10 60
         unset ${tab["$imi"]}
         continue
-    fi
-    
+    fi    
     if (whiptail --title "Creation des comptes SIP" --yesno "Verification: Nom:$Name / Extetion:$Exten / Password:$Password" 20 70 3>&1 1>&2 2>&3)
     then
       if(whiptail --title "Creation des comptes SIP" --yesno "Sauvegarder l'utilisateur: ${Name} ?" 10 60)
@@ -138,103 +135,15 @@ done
 SipUsers=$(echo "${tab[@]}")
 }
 
+# Fonction permettant d'afficher un message
 FailMes() {
   echo -e "\n ${Red} ### ${1} ### ${Neutral} \n"
+  logger -t "${0}" "${1}"
 }
 
 InfoMes() {
   echo -e "\n ${Green} ### ${1} ### ${Neutral} \n"
-}
-
-# Quand il y un exit, on efface les ressources que nous avons utilisées 
-Finish() {
-  for ErrExit in 'tar.gz.asc' 'sha256' 'tar.gz'
-  do
-    rm -rf "/usr/local/src/${VerAst}.${ErrExit}"
-  done
-}
-
-ExistInstall() {
-	Inx="$(dpkg -s ${1} | grep Status | awk '{print $2}')"
-
-	if [ "${Inx}" == "install" ]
-	then
-		FailMes "Attention ${1} est déjà installé"
-    exit 9
-	else 
-    type -a "${1}"
-    if [ "${?}" == "0" ]
-	  then
-		  FailMes "Attention ${1} est déjà installé"
-      exit 9
-	  else
-      test -d "/usr/local/src/${VerAst}"
-      if [ "${?}" == "0" ]
-	    then
-		    FailMes "Attention ${1} est déjà présent sur /usr/local/src/${VerAst}"
-        exit 9
-	    else
-		    InfoMes "Ok ${1} n'est pas installé sur cette machine !"
-      fi
-    fi    
-	fi
-}
-
-DownloadFile() {
-
-  for FileEx in 'tar.gz.asc' 'sha256' 'tar.gz'
-  do
-    wget -c -t 3 --progress=bar -O "/usr/local/src/${VerAst}.${FileEx}" "http://downloads.asterisk.org/pub/telephony/asterisk/releases/${VerAst}.${FileEx}" && sleep 1
-    if [ ${?} == 0 ]
-    then
-        InfoMes "Le téléchargement du fichier ${VerAst}.${FileEx} est terminé !"
-    else
-        FailMes "Erreur de téléchargement du fichier ${VerAst}.${FileEx} !"
-        exit 0
-    fi
-  done
-}
-
-TestAuthen() {
-  InfoMes "Test sha256sum :"
-  cd /usr/local/src/
-  sha256sum -c "/usr/local/src/${VerAst}.sha256"
-  if [ ${?} == 0 ]
-  then
-    InfoMes "vérification de la somme de contrôle SHA256 OK !"
-  else
-    FailMes "Erreur sur la vérification de la somme de contrôle SHA256 du fichier ${VerAst}.tar.gz !"
-    exit 1
-  fi
-  InfoMes "Test GPG :"
-  gpg2 --keyserver 'hkp://keyserver.ubuntu.com' --recv-keys "${KeyGpgAst}"
-  gpg2 --verify "/usr/local/src/${VerAst}.tar.gz.asc" "/usr/local/src/${VerAst}.tar.gz"
-  if [ ${?} == 0 ]
-  then
-    InfoMes "vérification de la clé GPG OK !"
-  else
-    FailMes "Erreur de vérification de la clé GPG du fichier ${VerAst}.tar.gz !"
-    exit 2
-  fi
-}
-
-ExtractFile() {
-  tar xzvf "/usr/local/src/${VerAst}.tar.gz"
-  if [ ${?} == 0 ]
-  then
-    InfoMes "Décompression OK !"
-  else
-    FailMes "Erreur de décompression du fichier ${VerAst}.tar.gz !"
-    exit 3
-  fi
-}
-
-#Fonction permettant d'afficher un message, usage: MesInfo [couleur] [message]
-MesInfo() {
-echo -e "\n $orange ------------------------------------------------------- $neutral \n"
-echo -e " $1           $2           $neutral "
-echo -e "\n $orange ------------------------------------------------------- $neutral \n"
-logger -t "${0}" "${2}"
+  logger -t "${0}" "${1}"
 }
 
 # Quand il y un exit, on efface les ressources que nous avons utilisées 
@@ -283,161 +192,38 @@ Finish() {
   rm -rf /usr/local/src/${VerLibpri}
   
   # Désinstallation d'applications
-  apt remove whiptail --purge
+  apt remove whiptail -y
+  apt remove dialog -y
 }
 
 ExistInstall() {
-  Inx="$(dpkg -s ${1} | grep Status | awk '{print $2}')"
-  if [ "${Inx}" == "install" ]
-  then
-    MesInfo $red "Attention ${1} est déjà installé"
-    exit 1
-  else 
+	Inx="$(dpkg -s ${1} | grep Status | awk '{print $2}')"
+
+	if [ "${Inx}" == "install" ]
+	then
+		FailMes "Attention ${1} est déjà installé"
+    exit 9
+	else 
     type -a "${1}"
     if [ "${?}" == "0" ]
-    then
-      MesInfo $red "Attention ${1} est déjà installé"
-      exit 1
-    else
+	  then
+		  FailMes "Attention ${1} est déjà installé"
+      exit 9
+	  else
       test -d "/usr/local/src/${VerAst}"
       if [ "${?}" == "0" ]
-      then
-        MesInfo $red "Attention ${1} est déjà présent sur /usr/local/src/"
-        exit 1
-      else
-        MesInfo $green "Ok ${1} n'est pas installé sur cette machine !"
+	    then
+		    FailMes "Attention ${1} est déjà présent sur /usr/local/src/${VerAst}"
+        exit 9
+	    else
+		    InfoMes "Ok ${1} n'est pas installé sur cette machine !"
       fi
-    fi      
-  fi
+    fi    
+	fi
 }
 
 InstallAst() {
-  set -e
-
-CountryCode="33"
-
-apt update && apt full-upgrade -y
-apt install wget -y
-apt install gpg -y
-apt install vim -y
-apt install dialog -y
-apt install apt-utils -y
-apt install systemd -y
-apt install ufw -y
-ufw enable
-ufw status
-
-wget https://downloads.asterisk.org/pub/telephony/asterisk/releases/asterisk-16.7.0.tar.gz -O /usr/local/src/asterisk-16.7.0.tar.gz
-wget https://downloads.asterisk.org/pub/telephony/asterisk/releases/asterisk-16.7.0.sha256 -O /usr/local/src/asterisk-16.7.0.sha256
-wget https://downloads.asterisk.org/pub/telephony/asterisk/releases/asterisk-16.7.0.tar.gz.asc -O /usr/local/src/asterisk-16.7.0.tar.gz.asc
-
-cd /usr/local/src/
-sha256sum -c /usr/local/src/asterisk-16.7.0.sha256
-gpg --keyserver 'hkp://keyserver.ubuntu.com' --recv-keys '5D984BE337191CE7'
-gpg --verify /usr/local/src/asterisk-16.7.0.tar.gz.asc /usr/local/src/asterisk-16.7.0.tar.gz
-
-tar xzvf /usr/local/src/asterisk-16.7.0.tar.gz
-echo "libvpb1 libvpb1/countrycode select $CountryCode" | debconf-set-selections
-yes | bash /usr/local/src/asterisk-16.7.0/contrib/scripts/install_prereq install
-cd /usr/local/src/asterisk-16.7.0/
-./configure
-
-make menuselect.makeopts
-/usr/local/src/asterisk-16.7.0/menuselect/menuselect --enable-category MENUSELECT_ADDONS menuselect.makeopts
-/usr/local/src/asterisk-16.7.0/menuselect/menuselect --disable CORE-SOUNDS-EN-GSM menuselect.makeopts
-/usr/local/src/asterisk-16.7.0/menuselect/menuselect --enable CORE-SOUNDS-FR-WAV menuselect.makeopts
-/usr/local/src/asterisk-16.7.0/menuselect/menuselect --enable CORE-SOUNDS-FR-ULAW menuselect.makeopts
-/usr/local/src/asterisk-16.7.0/menuselect/menuselect --enable CORE-SOUNDS-FR-ALAW menuselect.makeopts
-/usr/local/src/asterisk-16.7.0/menuselect/menuselect --enable MOH-OPSOUND-ULAW menuselect.makeopts
-/usr/local/src/asterisk-16.7.0/menuselect/menuselect --enable MOH-OPSOUND-ALAW menuselect.makeopts 
-/usr/local/src/asterisk-16.7.0/menuselect/menuselect --enable EXTRA-SOUNDS-FR-WAV menuselect.makeopts
-/usr/local/src/asterisk-16.7.0/menuselect/menuselect --enable EXTRA-SOUNDS-FR-ULAW menuselect.makeopts
-/usr/local/src/asterisk-16.7.0/menuselect/menuselect --enable EXTRA-SOUNDS-FR-ALAW menuselect.makeopts
-
-contrib/scripts/get_mp3_source.sh
-make
-make install
-make samples
-make config
-ldconfig
-
-groupadd asterisk
-useradd -r -d /var/lib/asterisk -g asterisk asterisk
-usermod -aG audio,dialout asterisk
-chown -R asterisk.asterisk /etc/asterisk
-chown -R asterisk.asterisk /var/{lib,log,spool}/asterisk
-chown -R asterisk.asterisk /usr/lib/asterisk
-
-#Créer une sauvegarde des fichiers de configurations
-cp /etc/asterisk/sip.conf /etc/asterisk/sip.conf.back
-cp /etc/asterisk/users.conf /etc/asterisk/users.conf.back
-cp /etc/asterisk/extensions.conf /etc/asterisk/extensions.conf.back
-cp /etc/asterisk/voicemail.conf /etc/asterisk/voicemail.conf.back 
-
-sed -i -e 's/^#AST_USER="asterisk"/AST_USER="asterisk"/g' /etc/default/asterisk
-sed -i -e 's/^#AST_GROUP="asterisk"/AST_GROUP="asterisk"/g' /etc/default/asterisk
-
-sed -i -e 's/^;runuser = asterisk/runuser = asterisk/g' /etc/asterisk/asterisk.conf
-sed -i -e 's/^;rungroup = asterisk/rungroup = asterisk/g' /etc/asterisk/asterisk.conf
-
-sed -i -e 's/^;languageprefix = yes/languageprefix = yes/g' /etc/asterisk/asterisk.conf
-
-sed -i -e 's/^;defaultlanguage = en/defaultlanguage = fr/g' /etc/asterisk/asterisk.conf
-sed -i -e 's/^documentation_language = en_US/documentation_language = fr_FR/g' /etc/asterisk/asterisk.conf
-
-sed -i -e 's/^;language=en/language=fr/g' /etc/asterisk/sip.conf
-sed -i -e 's/^;tonezone=se/tonezone=fr/g' /etc/asterisk/sip.conf
-
-sed -i -e 's/^enabled = yes/enabled = no/g' /etc/asterisk/ari.conf
-
-/etc/init.d/asterisk start
-
-systemctl enable asterisk
-
-ufw allow proto tcp from any to any port 5060,5061
-
-/etc/init.d/asterisk status
-}
-
-InstallDahdi() {
-  set -e
-apt-get install linux-headers-$(uname -r)
-wget https://downloads.asterisk.org/pub/telephony/dahdi-linux-complete/releases/dahdi-linux-complete-3.1.0+3.1.0.tar.gz -O /usr/local/src/dahdi-linux-complete-3.1.0+3.1.0.tar.gz
-wget https://downloads.asterisk.org/pub/telephony/dahdi-linux-complete/releases/dahdi-linux-complete-3.1.0+3.1.0.tar.gz.sha1 -O /usr/local/src/dahdi-linux-complete-3.1.0+3.1.0.tar.gz.sha1
-
-cd /usr/local/src/
-sha1sum -c dahdi-linux-complete-3.1.0+3.1.0.tar.gz.sha1 >> info.txt
-
-tar xzvf /usr/local/src/dahdi-linux-complete-3.1.0+3.1.0.tar.gz
-cd /usr/local/src/dahdi-linux-complete-3.1.0+3.1.0/ 
-make
-make install
-make config
-
-lsmod | grep dahdi
-
-/etc/init.d/dadhi start
-
-/etc/init.d/asterisk status
-}
-
-InstallLibpri() {
-  set -e
-
-wget https://downloads.asterisk.org/pub/telephony/libpri/releases/libpri-1.6.0.sha256 -O /usr/local/src/libpri-1.6.0.sha256
-wget https://downloads.asterisk.org/pub/telephony/libpri/releases/libpri-1.6.0.tar.gz -O /usr/local/src/libpri-1.6.0.tar.gz
-
-cd /usr/local/src/
-sha256sum -c libpri-1.6.0.sha256
-
-tar xzvf /usr/local/src/libpri-1.6.0.tar.gz
-
-cd /usr/local/src/libpri-1.6.0
-make
-make install
-}
-
-DownloadFile() {
+  cd /usr/local/src/
 
   for FileEx in 'tar.gz.asc' 'sha256' 'tar.gz'
   do
@@ -450,9 +236,7 @@ DownloadFile() {
         exit 0
     fi
   done
-}
 
-TestAuthen() {
   InfoMes "Test sha256sum :"
   cd /usr/local/src/
   sha256sum -c "/usr/local/src/${VerAst}.sha256"
@@ -473,9 +257,7 @@ TestAuthen() {
     FailMes "Erreur de vérification de la clé GPG du fichier ${VerAst}.tar.gz !"
     exit 2
   fi
-}
 
-ExtractFile() {
   tar xzvf "/usr/local/src/${VerAst}.tar.gz"
   if [ ${?} == 0 ]
   then
@@ -484,25 +266,153 @@ ExtractFile() {
     FailMes "Erreur de décompression du fichier ${VerAst}.tar.gz !"
     exit 3
   fi
+
+  echo "libvpb1 libvpb1/countrycode select $CountryCode" | debconf-set-selections
+  yes | bash /usr/local/src/asterisk-16.7.0/contrib/scripts/install_prereq install
+  cd /usr/local/src/asterisk-16.7.0/
+  ./configure
+
+  make menuselect.makeopts
+  /usr/local/src/asterisk-16.7.0/menuselect/menuselect --enable-category MENUSELECT_ADDONS menuselect.makeopts
+  /usr/local/src/asterisk-16.7.0/menuselect/menuselect --disable CORE-SOUNDS-EN-GSM menuselect.makeopts
+  /usr/local/src/asterisk-16.7.0/menuselect/menuselect --enable CORE-SOUNDS-FR-WAV menuselect.makeopts
+  /usr/local/src/asterisk-16.7.0/menuselect/menuselect --enable CORE-SOUNDS-FR-ULAW menuselect.makeopts
+  /usr/local/src/asterisk-16.7.0/menuselect/menuselect --enable CORE-SOUNDS-FR-ALAW menuselect.makeopts
+  /usr/local/src/asterisk-16.7.0/menuselect/menuselect --enable MOH-OPSOUND-ULAW menuselect.makeopts
+  /usr/local/src/asterisk-16.7.0/menuselect/menuselect --enable MOH-OPSOUND-ALAW menuselect.makeopts 
+  /usr/local/src/asterisk-16.7.0/menuselect/menuselect --enable EXTRA-SOUNDS-FR-WAV menuselect.makeopts
+  /usr/local/src/asterisk-16.7.0/menuselect/menuselect --enable EXTRA-SOUNDS-FR-ULAW menuselect.makeopts
+  /usr/local/src/asterisk-16.7.0/menuselect/menuselect --enable EXTRA-SOUNDS-FR-ALAW menuselect.makeopts
+
+  contrib/scripts/get_mp3_source.sh
+  make
+  make install
+  make samples
+  make config
+  ldconfig
+
+  groupadd asterisk
+  useradd -r -d /var/lib/asterisk -g asterisk asterisk
+  usermod -aG audio,dialout asterisk
+  chown -R asterisk.asterisk /etc/asterisk
+  chown -R asterisk.asterisk /var/{lib,log,spool}/asterisk
+  chown -R asterisk.asterisk /usr/lib/asterisk
+
+  #Créer une sauvegarde des fichiers de configurations
+  cp /etc/asterisk/sip.conf /etc/asterisk/sip.conf.back
+  cp /etc/asterisk/users.conf /etc/asterisk/users.conf.back
+  cp /etc/asterisk/extensions.conf /etc/asterisk/extensions.conf.back
+  cp /etc/asterisk/voicemail.conf /etc/asterisk/voicemail.conf.back 
+
+  sed -i -e 's/^#AST_USER="asterisk"/AST_USER="asterisk"/g' /etc/default/asterisk
+  sed -i -e 's/^#AST_GROUP="asterisk"/AST_GROUP="asterisk"/g' /etc/default/asterisk
+
+  sed -i -e 's/^;runuser = asterisk/runuser = asterisk/g' /etc/asterisk/asterisk.conf
+  sed -i -e 's/^;rungroup = asterisk/rungroup = asterisk/g' /etc/asterisk/asterisk.conf
+
+  sed -i -e 's/^;languageprefix = yes/languageprefix = yes/g' /etc/asterisk/asterisk.conf
+
+  sed -i -e 's/^;defaultlanguage = en/defaultlanguage = fr/g' /etc/asterisk/asterisk.conf
+  sed -i -e 's/^documentation_language = en_US/documentation_language = fr_FR/g' /etc/asterisk/asterisk.conf
+
+  sed -i -e 's/^;language=en/language=fr/g' /etc/asterisk/sip.conf
+  sed -i -e 's/^;tonezone=se/tonezone=fr/g' /etc/asterisk/sip.conf
+
+  sed -i -e 's/^enabled = yes/enabled = no/g' /etc/asterisk/ari.conf
+
+  /etc/init.d/asterisk start
+  systemctl enable asterisk
+  /etc/init.d/asterisk status
 }
 
-### Fonctions ###
+InstallDahdi() {
+  apt-get install linux-headers-$(uname -r)
+  for FileEx in 'tar.gz.sha1' 'tar.gz'
+  do
+    wget -c -t 3 --progress=bar -O "/usr/local/src/${VerDahdi}.${FileEx}" "https://downloads.asterisk.org/pub/telephony/dahdi-linux-complete/releases/${VerDahdi}.${FileEx}" && sleep 1
+    if [ ${?} == 0 ]
+    then
+        InfoMes "Le téléchargement du fichier ${VerDahdi}.${FileEx} est terminé !"
+    else
+        FailMes "Erreur de téléchargement du fichier ${VerLibpri}.${FileEx} !"
+        exit 0
+    fi
+  done
+
+  InfoMes "Test sha1 :"
+  cd /usr/local/src/
+  sha1sum -c ${VerDahdi}.tar.gz.sha1
+  if [ ${?} == 0 ]
+  then
+    InfoMes "vérification de la somme de contrôle SHA1 OK !"
+  else
+    FailMes "Erreur sur la vérification de la somme de contrôle SHA1 du fichier ${VerDahdi}.tar.gz !"
+    exit 1
+  fi
+  
+  tar xzvf "/usr/local/src/${VerDahdi}.tar.gz"
+  if [ ${?} == 0 ]
+  then
+    InfoMes "Décompression OK !"
+  else
+    FailMes "Erreur de décompression du fichier ${VerDahdi}.tar.gz !"
+    exit 3
+  fi
+
+  cd /usr/local/src/dahdi-linux-complete-3.1.0+3.1.0/ 
+  make
+  make install
+  make config
+
+  lsmod | grep dahdi
+  /etc/init.d/dadhi start
+  /etc/init.d/asterisk status
+}
+
+InstallLibpri() {
+  for FileEx in 'sha256' 'tar.gz'
+  do
+    wget -c -t 3 --progress=bar -O "/usr/local/src/${VerLibpri}.${FileEx}" "https://downloads.asterisk.org/pub/telephony/libpri/releases/${VerLibpri}.${FileEx}" && sleep 1
+    if [ ${?} == 0 ]
+    then
+        InfoMes "Le téléchargement du fichier ${VerLibpri}.${FileEx} est terminé !"
+    else
+        FailMes "Erreur de téléchargement du fichier ${VerLibpri}.${FileEx} !"
+        exit 0
+    fi
+  done
+
+  InfoMes "Test sha256 :"
+  cd /usr/local/src/
+  sha1sum -c ${VerLibpri}.sha256
+  if [ ${?} == 0 ]
+  then
+    InfoMes "vérification de la somme de contrôle SHA256 OK !"
+  else
+    FailMes "Erreur sur la vérification de la somme de contrôle SHA256 du fichier ${VerLibpri}.tar.gz !"
+    exit 1
+  fi
+  
+  tar xzvf "/usr/local/src/${VerLibpri}.tar.gz"
+  if [ ${?} == 0 ]
+  then
+    InfoMes "Décompression OK !"
+  else
+    FailMes "Erreur de décompression du fichier ${VerLibpri}.tar.gz !"
+    exit 3
+  fi
+
+  cd /usr/local/src/libpri-1.6.0
+  make
+  make install
+}
+
 FailMes() {
   echo -e "\n ${Red} ### ${1} ### ${Neutral} \n"
 }
 
 InfoMes() {
   echo -e "\n ${Green} ### ${1} ### ${Neutral} \n"
-}
-
-DownloadFile() {
-for Tools in "${VerAsterisk}" "${VerLibpri}"
-do
-  for Ext in 'tar.gz.asc' 'sha256' 'tar.gz'
-  do
-    wget -c -t 3 --progress=bar -O "/usr/local/src/${Tools}.${Ext}" "${DownloadAsterisk}/${Tools}.${Ext}" && sleep 1
-  done
-done
 }
 
 ### Code ###
@@ -523,6 +433,13 @@ then
 fi 
 
 # Installations d'applications
+apt update && apt full-upgrade -y
+apt install wget -y
+apt install gpg -y
+apt install vim -y
+apt install dialog -y
+apt install apt-utils -y
+apt install systemd -y
 apt install whiptail -y
 apt install sudo -y
 apt install ufw -y
@@ -613,11 +530,7 @@ enabled = true
 sleep 1
 systemctl restart fail2ban
 
-
-
-
-
-
+#####################################################################################################################
 
 # Effacer les traces
 trap Finish EXIT
